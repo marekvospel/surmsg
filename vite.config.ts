fa1b19fb0ab9e8f002bd73815d519be2996e754b
Marek Vospěl <marek@vospel.cz>
2023-09-26 14:41:44 +0200

    feat: add docker deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import { defineConfig } from 'vite';
 import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
+	server: {
+		proxy: {
+			'/rpc': {
+				target: 'ws://localhost:8000/',
+				changeOrigin: true,
+				ws: true,
+			}
+		}
+	},
 	plugins: [
 		sveltekit(),
 		UnoCSS(),
